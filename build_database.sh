7bf56d5633d87747fbbb34e04c30db5afebeccdb
Samuel Verstraete <samuel.verstraete@gmail.com>
2016-03-29 15:30:15 +0200

    drop the sample database, add if exists to the drop table statement

--- build_database.sh
+++ build_database.sh
@@ -3,6 +3,6 @@
 # build_database.sh - create empty temperature database schema for to log temperature in.
 #
 # Tom Holderness 22/01/2012
-sqlite3 piTemps.db 'DROP TABLE temperature_records;'
+sqlite3 piTemps.db 'DROP TABLE IF EXISTS temperature_records;'
 sqlite3 piTemps.db 'CREATE TABLE temperature_records(unix_time bigint primary key, top real, bottom real, [in] real, out real);' 
 
